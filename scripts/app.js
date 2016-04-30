52b285c7268d0304f2caff00081720c5e192217d
Ilja Andrejev <iljandrejev@gmail.com>
2016-04-30 11:13:06 +0300

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ilja on 30.04.2016.
+ */
+ var app = angular.module('todoApp',[]);
---
+++ scripts/directives/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Ilja on 30.04.2016.
+ */
+
+var helloworld = app.directive('helloword',function(){
+   return {
+       template:'Test value'
+   }
+});
